821b758d750f7009c306743a8d5c88b2ef2d890c
podlaskialgorytm3 <live.m851@gmail.com>
2023-11-25 23:11:27 +0100

    Refactor handleInput function to update value state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Header } from "./components/Header"
 import { UserInput } from "./components/UserInput"
+import { useState } from "react"
 
 function App() {
+  const [value,setValue] = useState({
+    value: "",
+    title: ""
+  })
   const handleInput = (e,title) => {
-    return {
-        value: e.target.value,
-        title: title
-    }
+    setValue({
+      value: e.target.value,
+      title: title
+    })
   }
+  console.log(value)
   return (
       <>
         <Header/>
